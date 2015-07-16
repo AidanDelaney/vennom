b11580f54a9606518d48a7127923e922270a1faf
Aidan Delaney <aidan@ontologyengineering.org>
2015-07-16 11:39:08 +0100

    Added simple Venn 2 test.

---
+++ src/test/java/org/eulerdiagrams/vennom/apCircles/TestGraph.java
@@ -0,0 +1,69 @@
+package org.eulerdiagrams.vennom.apCircles;
+
+import static org.eulerdiagrams.vennom.apCircles.display.APCircleDisplay.*;
+
+import org.eulerdiagrams.vennom.apCircles.drawers.APForceModel;
+import org.eulerdiagrams.vennom.graph.*;
+import org.eulerdiagrams.vennom.graph.drawers.GraphDrawer;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestGraph {
+
+	static {
+        /*
+         * The lines until the <<< marker are junk that is necessary to set before you can use the VenNom graph drawer.
+         * It's not pretty, but without setting properties of the EdgeTypes you get NullPointerExceptions from the
+         * VenNom code.
+         */
+        FIXED = new EdgeType("fixed");
+        FIXED.setLineColor(Color.black);
+        FIXED.setTextColor(Color.black);
+        FIXED.setSelectedLineColor(Color.gray);
+        FIXED.setPriority(1020);
+
+        ATTRACTOR = new EdgeType("attractor");
+        ATTRACTOR.setLineColor(Color.green);
+        ATTRACTOR.setTextColor(Color.green);
+        ATTRACTOR.setSelectedLineColor(Color.gray);
+        ATTRACTOR.setPriority(1019);
+
+        REPULSOR = new EdgeType("repulsor");
+        REPULSOR.setLineColor(Color.red);
+        REPULSOR.setTextColor(Color.red);
+        REPULSOR.setSelectedLineColor(Color.gray);
+        REPULSOR.setPriority(1018);
+
+        Graph.DEFAULT_NODE_TYPE.setHeight(20);
+        Graph.DEFAULT_NODE_TYPE.setWidth(20);
+        Graph.DEFAULT_NODE_TYPE.setBorderColor(Color.WHITE);
+        Graph.DEFAULT_NODE_TYPE.setTextColor(Color.BLUE);
+        Graph.DEFAULT_EDGE_TYPE = FIXED;
+        // <<<
+    }
+
+    @Test
+    public void testVenn2() {
+        // Venn2 area spec
+        AreaSpecification as = new AreaSpecification("a 100.0\nb 100.0\nab 500");
+        JFrame frame = new JFrame();
+        GraphDrawer gd = new APForceModel();
+        APCirclePanel apc = new APCirclePanel(frame);
+        apc.setSpecification(as);
+        apc.addGraphDrawer(gd);
+
+        apc.setGraph(as.generateAugmentedIntersectionGraph());
+        gd.layout();
+
+        for(Node n: gd.getGraph().getNodes()) {
+            assertThat(n, is(not(nullValue())));
+            System.out.println(n.getContour() + ", " + n.getLabel() + ", " + n.getCentre().x + ", " + n.getCentre().y);
+        }
+    }
+}
